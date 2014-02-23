84610214d37a24e76f5aebe48484f709a8c0a211
Skrynkovskyy Igor <skrynkovskyy@gmail.com>
2014-02-23 20:48:21 +0200

    test

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 
-from simple_googleplay import leaderboard, search, developer, app, FREE, PAID
+from sgp import (leaderboard, search, developer,
+                 app, FREE, PAID)
 
 class Developer(unittest.TestCase):
     def test_not_exists_developer(self):

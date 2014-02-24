64c8ecf059814742c3e2086720d91f8bd43c560d
Skrynkovskyy Igor <skrynkovskyy@gmail.com>
2014-02-25 00:21:34 +0200

    change package name

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 from setuptools import setup
+from google_play import __version__
 
 setup(
-    name='sgp',
+    name='google_play',
     author='Igor Skrynkovskyy',
     author_email='skrynkovskyy@gmail.com',
-    description='Simple Google Play interface',
+    description='Google Play app fetcher',
     license="BSD",
-    keywords=('googleplay spider appinfo leaderboard'
-             'search developer apps appsinfo'),
-    url='https://github.com/h2rd/simple-googleplay',
-    version='0.1',
-    packages=['sgp'],
+    keywords=['googleplay', 'spider', 'appinfo', 'apps',
+              'appsinfo', 'crawler'],
+    url='https://github.com/h2rd/google_play',
+    version=__version__,
+    packages=['google_play'],
     install_requires=[
         'grab==0.4.13',
         'lxml==3.3.1',

41570050d067f3f0833827624c7ed01666737549
Skrynkovskyy Igor <skrynkovskyy@gmail.com>
2014-02-25 03:56:16 +0200

    prepare for pypi

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
+
 from setuptools import setup
-from google_play import __version__
 
 setup(
     name='google_play',
     author='Igor Skrynkovskyy',
     author_email='skrynkovskyy@gmail.com',
     description='Google Play app fetcher',
-    license="BSD",
-    keywords=['googleplay', 'spider', 'appinfo', 'apps',
-              'appsinfo', 'crawler'],
+    license="MIT",
     url='https://github.com/h2rd/google_play',
-    version=__version__,
+    version='0.1',
     packages=['google_play'],
-    install_requires=[
+    test_suite='tests',
+    install_requires=(
         'grab==0.4.13',
         'lxml==3.3.1',
         'requests==2.2.1'
-    ]
+    )
 )
